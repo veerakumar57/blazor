5b0b1cb149e9c44a23215e8d288a940b5f8209e5
veerakumar57 <veerakumar.govindan@zucisystems.com>
2025-01-03 17:34:43 +0530

    Removed unused codes

---
+++ BlazorRecapDemo/Models/GroupedCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorRecapDemo.Models
+{
+    public class GroupedCartItem
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
